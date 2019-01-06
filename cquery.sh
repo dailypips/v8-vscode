cc1b492628ba98e9616e1b8d09e3efcdc0cd586f
Xiaojun Gao <gaoxiaojun@jscnnet.com>
2019-01-06 15:49:33 +0800

    update to relative path

--- cquery.sh
+++ cquery.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-fetch v8
-cd v8
-gn gen out.gn/x64.debug
-ninja -j6 -C out.gn/x64.debug
-cd ..
 ninja -C v8/out.gn/x64.debug -t compdb cxx cc > compile_commands.json
 gsed -i 's;/Users/gaoxiaojun/turbofan/vscode-v8/;;' compile_commands.json
--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ cd v8
 git pull
 gclient sync
 gn gen out.gn/x64.debug
-ninja -j6 -C out.gn/x64.debug
+ninja -j4 -C out.gn/x64.debug
 cd ..
 ninja -C v8/out.gn/x64.debug -t compdb cxx cc > compile_commands.json
+gsed -i 's;/Users/gaoxiaojun/turbofan/vscode-v8/;;' compile_commands.json
